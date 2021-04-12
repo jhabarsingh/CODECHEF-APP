76df760a05bcbc9b1a277d30d832a0127ac369c4
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-04-12 23:25:53 +0530

    added serializers for contests

--- codechef/apis/views.py
+++ codechef/apis/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from .serializers import ContestsSerializer
 
-# Create your views here.
+class ListContests(APIView):
+    """
+    View to list all contests in the system.
+   	"""
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
