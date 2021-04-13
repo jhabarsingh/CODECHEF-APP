3a76b657809f0fe28bda46c0de107137e5f615bd
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-04-13 23:16:38 +0530

    created all the scrapers'

--- scrapers/announcements.py
+++ scrapers/announcements.py
@@ -8,4 +8,21 @@ html_content = requests.get(url).text
 
 soup = BeautifulSoup(html_content, "lxml")
 
-upcoming_contest = soup.find("div", attrs={"id": "upcoming-timer"}).find("a")
+announcements = soup.find_all("div", attrs={"class": "widget"})[3]
+
+announcements = announcements.find("ul", attrs={"class": "announcements"}).find_all("li")
+
+obj = {}
+obj["announcements"] = []
+
+for i in announcements:
+	data = {}
+
+	urls = i.find("a")
+
+	data["content"] = i.text.strip("\n")
+	data["url"] = urls["href"]
+	data["url_text"] = urls.text
+	obj["announcements"].append(data)
+
+print(json.dumps(obj, indent=3))
