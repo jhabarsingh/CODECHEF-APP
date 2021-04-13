cfd50bd31e913dd1b5abf38c8c0e93e3ea46dd17
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-04-14 01:26:51 +0530

    added urls for questions

--- codechef/apis/urls.py
+++ codechef/apis/urls.py
@@ -13,9 +13,9 @@ app_name = "apis"
 
 urlpatterns = [
     path('contests/', ListContests.as_view(), name="contests"),
-    path('details/', UsersDetail.as_view(), name="details"),
- 	path('details/questions/', QuestionsDetail.as_view(), name="questions"),
-    path('details/contests/', ContestsDetail.as_view(), name="contests"),
+    path('user/', UsersDetail.as_view(), name="details"),
+ 	path('user/questions/', QuestionsDetail.as_view(), name="questions"),
+    path('user/contests/', ContestsDetail.as_view(), name="contests"),
     path('announcements/', AnnouncementsDetail.as_view(), name="announcements"),
     path('upcoming-contest/', UpcomingContestDetail.as_view(), name="upcoming-contest")
 ]
